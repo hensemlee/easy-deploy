81ec6790505c61a909fcc5293c27a99c804a8167
李俊 <lijun@tezign.com>
2023-06-25 17:38:00 +0800

    fix import

--- src/main/java/com/hensemlee/util/CicdUtils.java
+++ src/main/java/com/hensemlee/util/CicdUtils.java
@@ -1,10 +1,7 @@
 package com.hensemlee.util;
 
-import cn.hutool.core.collection.CollUtil;
-import cn.hutool.http.HttpException;
 import cn.hutool.http.HttpRequest;
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.hensemlee.bean.resp.CicdProject;
